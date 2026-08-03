073664b2a3fd0430dda8d9a798242dbabd111348
andretraa <andretririzky123@gmail.com>
2026-08-03 15:59:19 +0700

    add database.sqlite to git and configure Vercel SQLite deployment

--- api/index.php
+++ api/index.php
@@ -14,5 +14,21 @@ foreach ($storageDirs as $dir) {
     }
 }
 
+// Copy SQLite database file to /tmp for writable SQLite access on Vercel
+$tmpDbPath = '/tmp/database.sqlite';
+$sourceDbPath = __DIR__ . '/../database/database.sqlite';
+
+if (!file_exists($tmpDbPath)) {
+    if (file_exists($sourceDbPath)) {
+        copy($sourceDbPath, $tmpDbPath);
+    } else {
+        touch($tmpDbPath);
+    }
+}
+
+putenv("DB_DATABASE={$tmpDbPath}");
+$_ENV['DB_DATABASE'] = $tmpDbPath;
+$_SERVER['DB_DATABASE'] = $tmpDbPath;
+
 // Forward request to Laravel's public/index.php
 require __DIR__ . '/../public/index.php';
